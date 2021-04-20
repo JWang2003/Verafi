f51b516f9f6340fee850c47b89e8e6353da5dd02
William <kaangwill@gmail.com>
2021-04-19 21:29:56 -0700

    Moved current main_activity xml to FactCheckPage

---
+++ app/src/main/java/com/example/misinformation/FactCheckPage.java
@@ -0,0 +1,14 @@
+package com.example.misinformation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FactCheckPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fact_check_page);
+    }
+}
