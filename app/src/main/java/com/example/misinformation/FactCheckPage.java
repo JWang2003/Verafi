94748b90e2bf3244d40713495cc8058ebc7b5c08
William <kaangwill@gmail.com>
2021-04-20 20:46:07 -0700

    Lesson page and quiz page, plus merges with jimmy

--- app/src/main/java/com/example/misinformation/FactCheckPage.java
+++ app/src/main/java/com/example/misinformation/FactCheckPage.java
@@ -3,12 +3,34 @@ package com.example.misinformation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FactCheckPage extends AppCompatActivity {
+
+    FactCheckAPI factCheck;
+    Button submitButton;
+    EditText claim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fact_check_page);
+
+        getSupportActionBar().hide();
+        claim = findViewById(R.id.input);
+        submitButton = findViewById(R.id.submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search();
+            }
+        });
+    }
+
+    private void search() {
+        factCheck = new FactCheckAPI(claim.getText().toString());
+        factCheck.search();
     }
 }
