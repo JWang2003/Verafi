62dadda30b3ddcdee2209c695654b7c0a5c2bec1
Harry Peng <pengzehao2005@gmail.com>
2021-05-02 23:18:49 -0700

    Fixed some xml, fact check fragment still has button bug

--- app/src/main/java/com/example/misinformation/FactCheckFragment.java
+++ app/src/main/java/com/example/misinformation/FactCheckFragment.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
